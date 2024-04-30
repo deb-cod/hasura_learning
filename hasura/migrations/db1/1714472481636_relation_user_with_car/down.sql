dc4a8925b4ff0c22e6d2a4aed1c96d978c84340a
deb-cod <83533796+deb-cod@users.noreply.github.com>
2024-04-30 15:56:55 +0530

    init

---
+++ hasura/migrations/db1/1714472481636_relation_user_with_car/down.sql
@@ -0,0 +1,23 @@
+-- Could not auto-generate a down migration.
+-- Please write an appropriate down migration for the SQL below:
+-- -- CREATE TABLE user_data (
+-- -- 	id INT PRIMARY KEY,
+-- -- 	first_name VARCHAR(50),
+-- -- 	last_name VARCHAR(50),
+-- -- 	email VARCHAR(100) UNIQUE,
+-- -- 	gender VARCHAR(50)
+-- -- );
+--
+-- -- create table car (
+-- -- 	id INT,
+-- -- 	car_make VARCHAR(50),
+-- -- 	car_model VARCHAR(50),
+-- -- 	car_model_year VARCHAR(50),
+-- -- 	car_vin VARCHAR(50),
+-- -- 	user_id INT
+-- -- );
+--
+-- ALTER TABLE car
+-- ADD CONSTRAINT fk_user_id
+-- FOREIGN KEY (user_id)
+-- REFERENCES user_data(id);
